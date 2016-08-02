99cfa93929bd457a739cf9ada3e629586b80b8a7
serenasraynald <serenas.raynald@gmail.com>
2016-08-02 11:35:03 +0800

    Updated: post CRUD

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
 	belongs_to :category
 	belongs_to :user
 	has_many :comments
+
+	validates :title, presence: true
+	validates :category_id, presence: true
+	validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 	has_many :posts
+
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :password_hash, presence: true
+	validates :password_hash, confirmation: true
 end
